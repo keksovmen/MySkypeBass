8827e6deea66d3bf141ca25db02d5137ebadc2a8
keksovmen <keksovmen@yandex.ru>
2023-06-12 11:31:46 +0300

    Syncing some old stashed changes about opus codec

--- src/test/java/TestCompression.java
+++ src/test/java/TestCompression.java
@@ -1,56 +1,120 @@
-import com.sun.jna.Native;
 import com.sun.jna.ptr.PointerByReference;
 import net.tomp2p.opuswrapper.Opus;
 
 import javax.sound.sampled.*;
-import java.io.File;
+import javax.swing.*;
+import java.awt.*;
+import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 import java.nio.ShortBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.stream.Stream;
 
 public class TestCompression {
 
-    static {
-        try {
-            System.loadLibrary("opus");
-        } catch (UnsatisfiedLinkError e) {
-            try {
-                File f = Native.extractFromResourcePath("opus");
-                System.load(f.getAbsolutePath());
-            } catch (Exception e1) {
-                e.printStackTrace();
-                e1.printStackTrace();
-            }
-        }
-    }
+    //    static {
+//        try {
+//            System.loadLibrary("opus");
+//        } catch (UnsatisfiedLinkError e) {
+//            try {
+//                File f = Native.extractFromResourcePath("opus");
+//                System.load(f.getAbsolutePath());
+//            } catch (Exception e1) {
+//                e.printStackTrace();
+//                e1.printStackTrace();
+//            }
+//        }
+//    }
+    private static TargetDataLine input;
+    private static SourceDataLine output;
 
-    private static final int FRAME_SIZE = 80;
+    private static Executor executor = Executors.newSingleThreadExecutor();
+
+    private static final int FRAME_SIZE = 640;
 
     public static void main(String[] args) throws LineUnavailableException {
+        int vals [] = new int[]{-3, -1, 0 , 1 , 3};
+//        Arrays.stream(vals).max().getAsInt()
+//        Collections.emptyList()
+        JFrame frame = new JFrame("TEST");
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        Canvas canvas = new Canvas();
+        Collections.max(Collections.emptyList(), (o1, o2) -> 1);
         AudioFormat audioFormat = new AudioFormat(16000f, 16, 1, true, true);
         assert testConversionFront();
         assert testConversionBack();
+        input = createTargetLine(audioFormat);
+        output = createSourceLine(audioFormat);
+        long encodeTimeAll = 0;
+        long decodeTimeAll = 0;
+        long iteration = 0;
         while (true) {
-            ShortBuffer readFromMic = readFromMic(audioFormat, 3);
-            play(audioFormat, shortsToByteBigEndian(readFromMic));
+            final ShortBuffer readFromMic = readFromMic(audioFormat, 0.120f, input);
+//            play(audioFormat, shortsToByteLittleEndian(readFromMic));
+            long timeBeforeNano = System.nanoTime();
             List<ByteBuffer> encoded = encode(audioFormat, readFromMic);
+            long timeResultMicro = (System.nanoTime() - timeBeforeNano) / 1000;
+            encodeTimeAll += timeResultMicro;
+
+            final int encodedSize = encoded.stream().mapToInt(Buffer::limit).sum();
+            timeBeforeNano = System.nanoTime();
             ShortBuffer decoded = decode(audioFormat, encoded);
-            play(audioFormat, shortsToByteBigEndian(decoded));
+            timeResultMicro = (System.nanoTime() - timeBeforeNano) / 1000;
+            decodeTimeAll += timeResultMicro;
+
+            iteration++;
+
+            System.out.printf("Initial size - %d\tEncoded - %d\tDecoded - %d\tEncode t - %d\tDecode t - %d\n",
+                    readFromMic.limit() * 2, encodedSize, decoded.limit() * 2, encodeTimeAll / iteration, decodeTimeAll / iteration);
+
+            executor.execute(() -> {
+                try {
+                    if (audioFormat.isBigEndian())
+                        play(audioFormat, shortsToByteBigEndian(readFromMic), output);
+                    else
+                        play(audioFormat, shortsToByteLittleEndian(readFromMic), output);
+                } catch (LineUnavailableException e) {
+                    e.printStackTrace();
+                }
+            });
+//            play(audioFormat, shortsToByteLittleEndian(decoded));
         }
     }
 
-    private static ShortBuffer readFromMic(AudioFormat audioFormat, int durationSec) throws LineUnavailableException {
+    private static ShortBuffer readFromMic(AudioFormat audioFormat, float durationSec) throws LineUnavailableException {
         TargetDataLine targetDataLine = AudioSystem.getTargetDataLine(audioFormat);
         targetDataLine.open(audioFormat);
         targetDataLine.start();
-
-        byte[] buffer = new byte[(int) (audioFormat.getSampleRate() * audioFormat.getFrameSize() * durationSec)];
+        int bufferSize = (int) (audioFormat.getSampleRate() * audioFormat.getFrameSize() * durationSec);
+        bufferSize = bufferSize - bufferSize % audioFormat.getFrameSize();
+        byte[] buffer = new byte[bufferSize];
         targetDataLine.read(buffer, 0, buffer.length);
         targetDataLine.close();
-        return bytesToShortBigEndian(buffer);
+//        return bytesToShortBigEndian(buffer);
+        if (audioFormat.isBigEndian())
+            return bytesToShortBigEndian(buffer);
+        else
+            return bytesToShortLittleEndian(buffer);
+    }
+
+    private static ShortBuffer readFromMic(AudioFormat audioFormat, float durationSec, TargetDataLine targetDataLine) throws LineUnavailableException {
+        int bufferSize = (int) (audioFormat.getSampleRate() * audioFormat.getFrameSize() * durationSec);
+        bufferSize = bufferSize - bufferSize % audioFormat.getFrameSize();
+        byte[] buffer = new byte[bufferSize];
+        targetDataLine.read(buffer, 0, buffer.length);
+//        return bytesToShortBigEndian(buffer);
+        if (audioFormat.isBigEndian())
+            return bytesToShortBigEndian(buffer);
+        else
+            return bytesToShortLittleEndian(buffer);
     }
 
     private static void play(AudioFormat audioFormat, byte[] data) throws LineUnavailableException {
@@ -62,6 +126,14 @@ public class TestCompression {
         sourceDataLine.close();
     }
 
+    private static void play(AudioFormat audioFormat, byte[] data, SourceDataLine sourceDataLine) throws LineUnavailableException {
+        int write = sourceDataLine.write(data, 0, data.length);
+        if (write != data.length) {
+            sourceDataLine.write(data, write, data.length - write);
+        }
+//        sourceDataLine.drain();
+    }
+
     private static List<ByteBuffer> encode(AudioFormat audioFormat, ShortBuffer raw) {
         IntBuffer error = IntBuffer.allocate(4);
         PointerByReference encoder = Opus.INSTANCE.opus_encoder_create(
@@ -71,15 +143,19 @@ public class TestCompression {
         List<ByteBuffer> byteBuffers = new ArrayList<>();
         int read = 0;
         while (raw.hasRemaining()) {
-            ByteBuffer bytes = ByteBuffer.allocate(1024);//tmp size may not work with every sample rate
+            ByteBuffer bytes = ByteBuffer.allocate(2048);//tmp size may not work with every sample rate
             int toRead = Math.min(raw.remaining(), bytes.remaining());
 
             read = Opus.INSTANCE.opus_encode(encoder, raw, FRAME_SIZE, bytes, toRead);
             if (read < 0)
-                throw new RuntimeException("Encoding failed");
+                throw new RuntimeException("Encoding failed - " + read);
             bytes.position(bytes.position() + read);
             bytes.flip();
             byteBuffers.add(bytes);
+            if (raw.position() + FRAME_SIZE > raw.capacity()) {
+                raw.position(raw.capacity());
+                continue;
+            }
             raw.position(raw.position() + FRAME_SIZE);
         }
         raw.flip();
@@ -116,6 +192,17 @@ public class TestCompression {
         return buffer;
     }
 
+    private static ShortBuffer bytesToShortLittleEndian(byte[] bytes) {
+        ShortBuffer buffer = ShortBuffer.allocate(bytes.length / 2);
+        for (int i = 0; i < bytes.length; i += 2) {
+            int high = bytes[i + 1] << 8;
+            int low = bytes[i] & 0xFF;
+            buffer.put((short) (high | low));
+        }
+        buffer.flip();
+        return buffer;
+    }
+
     private static byte[] shortsToByteBigEndian(ShortBuffer buffer) {
         byte[] bytes = new byte[buffer.limit() * 2];
         short[] shortArray = buffer.array();
@@ -129,6 +216,19 @@ public class TestCompression {
         return bytes;
     }
 
+    private static byte[] shortsToByteLittleEndian(ShortBuffer buffer) {
+        byte[] bytes = new byte[buffer.limit() * 2];
+        short[] shortArray = buffer.array();
+        for (int i = 0; i < buffer.limit(); i++) {
+            short s = shortArray[i];
+            byte high = (byte) (s >> 8);
+            byte low = (byte) s;
+            bytes[(i * 2) + 1] = high;
+            bytes[i * 2] = low;
+        }
+        return bytes;
+    }
+
     private static boolean testConversionFront() {
         byte[] test = new byte[]{2, -55};
         return bytesToShortBigEndian(test).get() == 712;
@@ -137,5 +237,30 @@ public class TestCompression {
     private static boolean testConversionBack() {
         ShortBuffer shortBuffer = ShortBuffer.wrap(new short[]{712});
         return Arrays.equals(shortsToByteBigEndian(shortBuffer), new byte[]{2, -55});
+    }
+
+    private static SourceDataLine createSourceLine(AudioFormat audioFormat) throws LineUnavailableException {
+        SourceDataLine line = AudioSystem.getSourceDataLine(audioFormat);
+        line.open(audioFormat);
+        line.start();
+        return line;
+    }
+
+    private static TargetDataLine createTargetLine(AudioFormat audioFormat) throws LineUnavailableException {
+        TargetDataLine line = AudioSystem.getTargetDataLine(audioFormat);
+        line.open(audioFormat);
+        line.start();
+        return line;
+    }
+
+    private static class JC extends JComponent{
+
+        @Override
+        protected void printComponent(Graphics g) {
+//            super.printComponent(g);
+            Graphics2D g2 = (Graphics2D) g;
+//            g2.drawOval();
+//            g2.drawLine();
+        }
     }
 }
